99526b4ed84ffd996a2db2cfc8209894680b18fa
NicuKing <40795268+NicuKing@users.noreply.github.com>
2018-11-15 10:04:17 +0100

    basic development navigation

--- app/src/main/java/com/example/admin/planungsapp/MainActivity.java
+++ app/src/main/java/com/example/admin/planungsapp/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.admin.planungsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnlogin;
+    Button btncreatep;
+    Button btnviewp;
+    Button btncalender;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btncalender = findViewById(R.id.btnCalender);
+
+        btnlogin = (Button) findViewById(R.id.btnLogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent login = new Intent(MainActivity.this, MainLogin.class);
+
+                startActivity(login);
+
+            }
+        });
+
+        btncreatep = (Button) findViewById(R.id.btnCreateProjects);
+        btncreatep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent createproject = new Intent(MainActivity.this, CreateProjects.class);
+
+                startActivity(createproject);
+
+            }
+        });
+
+        btnviewp = (Button) findViewById(R.id.btnViewProjects);
+        btnviewp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent viewprojects = new Intent(MainActivity.this, ListProjects.class);
+
+                startActivity(viewprojects);
+
+            }
+        });
+
+        btncalender = (Button) findViewById(R.id.btnCalender);
+        btncalender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent calender = new Intent(MainActivity.this, MainCalender.class);
+
+                startActivity(calender);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/admin/planungsapp/MainLogin.java
@@ -0,0 +1,13 @@
+package com.example.admin.planungsapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+    }
+}
