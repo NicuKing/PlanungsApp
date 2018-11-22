692f5facd18862cc9aa2d4b51d5e98f545f6ed3c
Rene Meisters <rene.meisters@bwdbern.ch>
2018-11-22 10:24:02 +0100

    Login ohne Sign Up und Logout

--- app/src/main/java/com/example/admin/planungsapp/MainLogin.java
+++ app/src/main/java/com/example/admin/planungsapp/MainLogin.java
@@ -1,13 +1,83 @@
 package com.example.admin.planungsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainLogin extends AppCompatActivity {
+
+    private EditText inp_email,inp_pwd;
+    private Button btn_login, btn_toSignUp;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        inp_email = (EditText)findViewById(R.id.txt_login_email);
+        inp_pwd = (EditText)findViewById(R.id.txt_login_pwd);
+        btn_login = (Button)findViewById(R.id.btn_login);
+        btn_toSignUp = (Button)findViewById(R.id.btn_toSignUp);
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null){
+                    startActivity(new Intent(MainLogin.this, HomeActivity.class));
+                }
+            }
+        };
+        btn_toSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainLogin.this,SignUpActivity.class));
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSignIn();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(authListener);
+    }
+
+    private void startSignIn(){
+        String email = inp_email.getText().toString();
+        String password = inp_pwd.getText().toString();
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(MainLogin.this, "Please fill all fields", Toast.LENGTH_LONG).show();
+        }
+        else {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(MainLogin.this, "Sign In Problem", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
     }
 }
